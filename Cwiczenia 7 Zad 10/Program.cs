c9375cfb4d43a62192f7c6f139847064be29a7bf
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-29 15:14:36 +0100

    Add files via upload

---
+++ Cwiczenia 7 Zad 10/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Cwiczenia_7_Zad_10
+{
+    class Funkcje
+    {
+        public int Zamiana(ref int x)
+        {
+            return x++ * 2;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b; 
+            Funkcje f1 = new Funkcje();
+            b = f1.Zamiana(ref a);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
